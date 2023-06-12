2623ebf718381d819fe4e71231fd651a9e7ab2e0
Elvan Hidayat <elvanhd@gmail.com>
2023-06-12 10:07:14 +0700

    create database connection

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
+dotenv.config();
+
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -18,4 +24,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
