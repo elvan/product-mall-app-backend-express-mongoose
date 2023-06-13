17f47388e2f574c4e08b4a4301984288c97f7577
Elvan Hidayat <elvanhd@gmail.com>
2023-06-14 00:33:00 +0700

    user routes and controller

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ connectDB();
 const app = express();
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
