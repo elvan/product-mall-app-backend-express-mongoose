b59722c8be4d451e851f53869855524c62b8671a
Elvan Hidayat <elvanhd@gmail.com>
2023-06-14 01:46:59 +0700

    auth endpoints and using auth middleware

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
