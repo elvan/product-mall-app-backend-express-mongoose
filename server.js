718e0f7d5ed55161d6d45da98c5635665ba9ce54
Elvan Hidayat <elvanhd@gmail.com>
2023-06-14 12:48:17 +0700

    order routes and controller

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import orderRoutes from './routes/orderRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
